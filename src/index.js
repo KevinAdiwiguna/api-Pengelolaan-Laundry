dd430544dbb6c6fd81e0560f219c4b2dfbc44705
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-04-12 15:51:23 +0800

    feat: add environment variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3001
+const port = process.env.PORT || 8888
 
 const usersRoutes = require('./routes/users')
 const outletRoutes = require('./routes/outlet')
