59e96ee56a01826f353a76e62189406903362570
KevinAdiwiguna <kevinadiwiguna.school@gmail.com>
2023-04-14 16:32:02 +0800

    fixed: blocked by CORS

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 8888
 
@@ -8,13 +9,13 @@ const outletRoutes = require('./routes/outlet')
 const paketRoutes = require('./routes/paket')
 const logRequest = require("./middleware/logs")
 
-
+app.use(cors())
 app.use(logRequest)
 app.use(express.json())
 
-app.use('/users', usersRoutes)
-app.use('/outlet', outletRoutes)
-app.use('/paket', paketRoutes)
+app.use('/api/users', usersRoutes)
+app.use('/api/outlet', outletRoutes)
+app.use('/api/paket', paketRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
